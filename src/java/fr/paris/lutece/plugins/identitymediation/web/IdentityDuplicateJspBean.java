125a3df13b00a0ffbd82b07a167f3f6297f5cf17
Clément Roure <croure@yupiik.com>
2023-05-02 17:15:46 +0300

    #WIP first UI

--- src/java/fr/paris/lutece/plugins/identitymediation/web/IdentityDuplicateJspBean.java
+++ src/java/fr/paris/lutece/plugins/identitymediation/web/IdentityDuplicateJspBean.java
@@ -33,11 +33,20 @@
  */
 package fr.paris.lutece.plugins.identitymediation.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.paris.lutece.plugins.identitystore.v3.web.rs.dto.search.QualifiedIdentity;
+import fr.paris.lutece.plugins.identitystore.web.exception.IdentityStoreException;
+import fr.paris.lutece.portal.service.util.AppLogService;
 import fr.paris.lutece.portal.util.mvc.admin.MVCAdminJspBean;
 import fr.paris.lutece.portal.util.mvc.admin.annotations.Controller;
 import fr.paris.lutece.portal.util.mvc.commons.annotations.View;
+import org.apache.commons.collections.CollectionUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 /**
  * This class provides the user interface to manage identity duplicates (search, resolve)
@@ -45,6 +54,10 @@ import javax.servlet.http.HttpServletRequest;
 @Controller( controllerJsp = "IdentityDuplicate.jsp", controllerPath = "jsp/admin/plugins/identitymediation/", right = "IDENTITYMEDIATION_MANAGEMENT" )
 public class IdentityDuplicateJspBean extends MVCAdminJspBean
 {
+    // Messages
+    private static final String MESSAGE_FETCH_POTENTIAL_DUPLICATES_ERROR = "identitymediation.message.fetch_duplicates.error";
+    private static final String MESSAGE_FETCH_POTENTIAL_DUPLICATES_NORESULT = "identitymediation.message.fetch_duplicates.noresult";
+
     // Views
     private static final String VIEW_SEARCH_DUPLICATES = "searchDuplicates";
     private static final String VIEW_RESOLVE_DUPLICATES = "resolveDuplicates";
@@ -57,6 +70,9 @@ public class IdentityDuplicateJspBean extends MVCAdminJspBean
     private static final String PROPERTY_PAGE_TITLE_SEARCH_DUPLICATES = "identitymediation.search_duplicates.pageTitle";
     private static final String PROPERTY_PAGE_TITLE_RESOLVE_DUPLICATES = "identitymediation.resolve_duplicates.pageTitle";
 
+    // Markers
+    private static final String MARK_POTENTIAL_DUPLICATE_LIST = "potential_duplicate_list";
+
     /**
      * Process the data to send the search request and returns the duplicates search form and results
      *
@@ -65,9 +81,47 @@ public class IdentityDuplicateJspBean extends MVCAdminJspBean
      * @return the html code of the duplicate form
      */
     @View( value = VIEW_SEARCH_DUPLICATES, defaultView = true )
-    public String getSearchDuplicates( HttpServletRequest request )
+    public String getSearchDuplicates( final HttpServletRequest request )
     {
-        return getPage( PROPERTY_PAGE_TITLE_SEARCH_DUPLICATES, TEMPLATE_SEARCH_DUPLICATES );
+        final List<QualifiedIdentity> identities = new ArrayList<>( );
+        try
+        {
+            identities.addAll( fetchPotentialDuplicates( ) );
+            if ( CollectionUtils.isEmpty( identities ) )
+            {
+                addInfo( MESSAGE_FETCH_POTENTIAL_DUPLICATES_NORESULT, getLocale( ) );
+            }
+        }
+        catch( final IdentityStoreException e )
+        {
+            AppLogService.error( "Error while fetching potential identity duplicates.", e );
+            addError( MESSAGE_FETCH_POTENTIAL_DUPLICATES_ERROR, getLocale( ) );
+        }
+
+        final Map<String, Object> model = getModel( );
+        model.put( MARK_POTENTIAL_DUPLICATE_LIST, identities );
+
+        return getPage( PROPERTY_PAGE_TITLE_SEARCH_DUPLICATES, TEMPLATE_SEARCH_DUPLICATES, model );
+    }
+
+    /**
+     * Fetches identities that are likely to have duplicates.
+     */
+    private List<QualifiedIdentity> fetchPotentialDuplicates( ) throws IdentityStoreException
+    {
+        // FIXME mock for the time being.
+        try
+        {
+            final ObjectMapper mapper = new ObjectMapper( );
+            final QualifiedIdentity id = mapper.readValue(
+                    "{\"scoring\":1,\"quality\":80,\"coverage\":80,\"connection_id\":\"mock-connection-id\",\"customer_id\":\"mock-cuid\",\"attributes\":[{\"key\":\"birthdate\",\"value\":\"01/01/1990\",\"type\":\"string\",\"certificationLevel\":400,\"certifier\":\"pj\",\"certificationDate\":\"2023-05-02\"},{\"key\":\"family_name\",\"value\":\"Dupont\",\"type\":\"string\",\"certificationLevel\":700,\"certifier\":\"fc\",\"certificationDate\":\"2023-05-02\"},{\"key\":\"first_name\",\"value\":\"Jean\",\"type\":\"string\",\"certificationLevel\":700,\"certifier\":\"fc\",\"certificationDate\":\"2023-05-02\"}]}",
+                    QualifiedIdentity.class );
+            return Collections.singletonList( id );
+        }
+        catch( Exception e )
+        {
+            throw new IdentityStoreException( "error", e );
+        }
     }
 
     /**
@@ -78,7 +132,7 @@ public class IdentityDuplicateJspBean extends MVCAdminJspBean
      * @return the html code of the form
      */
     @View( value = VIEW_RESOLVE_DUPLICATES )
-    public String getResolveDuplicates( HttpServletRequest request )
+    public String getResolveDuplicates( final HttpServletRequest request )
     {
         return getPage( PROPERTY_PAGE_TITLE_RESOLVE_DUPLICATES, TEMPLATE_RESOLVE_DUPLICATES );
     }
