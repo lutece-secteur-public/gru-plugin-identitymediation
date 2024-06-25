3199942045c1ea6c5c07134f9b84f8384ef3a2d2
seboo <25958061+seboo@users.noreply.github.com>
2024-06-25 18:10:19 +0200

    LUT-28211 : lower plugin rights level

--- src/sql/plugins/identitymediation/core/init_core_identitymediation.sql
+++ src/sql/plugins/identitymediation/core/init_core_identitymediation.sql
@@ -4,7 +4,7 @@
 --
 DELETE FROM core_admin_right WHERE id_right = 'IDENTITYMEDIATION_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES
-    ('IDENTITYMEDIATION_MANAGEMENT','identitymediation.adminFeature.IdentityDuplicate.name',1,'jsp/admin/plugins/identitymediation/IdentityDuplicate.jsp','identitymediation.adminFeature.IdentityDuplicate.description',0,'identitymediation',NULL,NULL,NULL,4);
+    ('IDENTITYMEDIATION_MANAGEMENT','identitymediation.adminFeature.IdentityDuplicate.name',3,'jsp/admin/plugins/identitymediation/IdentityDuplicate.jsp','identitymediation.adminFeature.IdentityDuplicate.description',0,'identitymediation',NULL,NULL,NULL,4);
 
 
 --
